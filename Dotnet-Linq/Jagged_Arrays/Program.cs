f2ac91cac0f06fd85fd3db5d5091f87d2a70c38c
farleybsd <farley.t.i@hotmail.com>
2021-02-17 20:57:19 -0300

    Jagged Array = Array Denteando = Array Serrilhado

---
+++ Dotnet-Linq/Jagged_Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jagged_Arrays
+{
+    class Program
+    {
+        // Jagged Array = Array Denteando = Array Serrilhado
+        static void Main(string[] args)
+        {
+            string[][] familia = new string[3][];
+            //{
+            //    { "Fred","Wilma","Pedrita" },
+            //    { "Homer","Marge","Lisa","Bart","Maggie" },
+            //    { "Florinda","Kiko"}
+            //};
+
+            familia[0] = new string[] { "Fred", "Wilma", "Pedrita" };
+            familia[1] = new string[] { "Homer", "Marge", "Lisa", "Bart", "Maggie" };
+            familia[2] = new string[] { "Florinda", "Kiko" };
+            Imprimir(familia);
+
+            Console.ReadKey();
+        }
+
+        private static void Imprimir(string[][] familia)
+        {
+            foreach (var familiaitem in familia)
+            {
+                Console.WriteLine(string.Join(",", familiaitem));
+            }
+        }
+
+    }
+}
